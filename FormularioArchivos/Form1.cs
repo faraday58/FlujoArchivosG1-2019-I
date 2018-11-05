c221c187b4516c489d86295d0d1f90aac79a0a55
faraday58 <jarodriguezvera@gmail.com>
2018-11-05 12:30:04 -0600

    Binario

--- FormularioArchivos/Form1.cs
+++ FormularioArchivos/Form1.cs
@@ -22,37 +22,63 @@ namespace FormularioArchivos
         private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "*.txt,*.bin| txt,bin";
+            ofd.Filter = "Texto |*.txt | Binarios| *.bin";
 
             if( ofd.ShowDialog() == DialogResult.OK    )
             {
                 texto = new List<string>();
-                try
+
+                if (ofd.Filter.Contains("*.bin"))
                 {
-                    fs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read);
-                    sr = new StreamReader(fs);
-                    string aux = sr.ReadLine();
-                    while( aux != null   )
+                    try
                     {
-                        texto.Add(aux);
-                        aux=sr.ReadLine();
+                        fi = new FileInfo(ofd.FileName);
+                        br = new BinaryReader(fi.OpenRead());
+                        rtbNotas.AppendText(br.ReadString());
+                        rtbNotas.AppendText(br.ReadInt32().ToString());
+                        rtbNotas.AppendText(br.ReadDouble().ToString());
+
+
+                    }
+                    catch(IOException error)
+                    {
+                        MessageBox.Show(error.Message);
+                    }
+                    finally
+                    {
+                        br.Close();
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        fs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read);
+                        sr = new StreamReader(fs);
+                        string aux = sr.ReadLine();
+                        while (aux != null)
+                        {
+                            texto.Add(aux);
+                            aux = sr.ReadLine();
+                        }
+
                     }
 
-                }
-                catch(IOException error)
-                {
-                    MessageBox.Show("Error :" + error.Message);
-                }
-                finally
-                {
-                    fs.Close();
-                    sr.Close();
-                }
-                for(int i=0; i < texto.Count;i ++)
-                {
-                    rtbNotas.AppendText(texto[i]);
-                }                
+                    catch (IOException error)
+                    {
+                        MessageBox.Show("Error :" + error.Message);
+                    }
+                    finally
+                    {
+                        fs.Close();
+                        sr.Close();
+                    }
 
+                    for (int i = 0; i < texto.Count; i++)
+                    {
+                        rtbNotas.AppendText(texto[i]);
+                    }
+                }
             }
 
 
